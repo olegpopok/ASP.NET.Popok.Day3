7c225bb79954f72372bd69404b43e089e526623c
olegpopok <olegpopo1994@gmail.com>
2015-07-17 13:32:52 +0300

    0.0

---
+++ Task1.Library/Polynomial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Library
+{
+    public sealed class Polynomial
+    {
+        public double Variable { get; set; }
+        private double[] coefficients;
+
+
+        public double[] Coefficients 
+        {
+            get { return this.coefficients; }
+            set 
+            {
+                if (value.Length != 0 && value[value.Length - 1] != 0)
+                    this.coefficients = value;
+            }
+        }
+
+        public int Power 
+        {
+            get { return coefficients.Length; }
+        }
+
+        
+    }
+}
